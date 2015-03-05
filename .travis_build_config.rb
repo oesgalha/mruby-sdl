03d7243be59fc15c989800c14cc9625292f7862e
oesgalha <oscaresgalha@gmail.com>
2015-03-04 23:03:30 -0300

    Fixed build. Returning window ID in the window_create function

--- .travis_build_config.rb
+++ .travis_build_config.rb
@@ -1,5 +1,5 @@
 MRuby::Build.new do |conf|
   toolchain :gcc
   conf.gembox 'default'
-  conf.gembox "#{ENV["TRAVIS_BUILD_DIR"]}/sdl"
+  conf.gem "#{ENV["TRAVIS_BUILD_DIR"]}"
 end
--- src/videos.c
+++ src/videos.c
@@ -8,7 +8,7 @@ mrb_sdl_create_window(mrb_state *mrb, mrb_value self)
   mrb_int a, b, width, height, show;
   mrb_get_args(mrb, "ziiiii", &window_title, &a, &b, &width, &height, &show);
   SDL_Window* window = SDL_CreateWindow(window_title, a, b, width, height, show);
-  return mrb_obj_value(window);
+  return mrb_fixnum_value(SDL_GetWindowID(window));
 }
 
 void
